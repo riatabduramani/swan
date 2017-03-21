ca312311948095e0b58bfa4ed2575b1f60baaa58
riatabduramani <riat.abduramani@gmail.com>
2017-03-21 19:50:51 +0100

    changing header

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -29,81 +29,9 @@
         <div class="preloader_spinner"></div>
     </div>
     <div class="home_page">
-<<<<<<< HEAD
-<!-- ==========================================================
-    1.*header_area start
-============================================================ -->
-        <header class="romana_header">
-            <div class="hrader_top_area">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-sm-2 col-xs-12">
-                            <div class="logo">
-                                <a href="index.html"><img src="images/logo.png" alt="logo"></a>
-                            </div>
-                        </div>
-                        <div class="col-sm-7 col-md-offset-1">
-                            <div class="header_top_left">
-                                <ul>
-                                    <li><span class="fa fa-phone"></span>(000) 11 222 3333</li>
-                                    <li><span class="fa fa-envelope"></span>info@swan.mk</li>
-                                    <!--<li><span class="fa fa-map-marker"></span></li>-->
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col-md-2 col-sm-3 col-xs-12">
-                            <div class="header_top_right">
-                             @if (Auth::check())
-                             @role('admin')
-                                <a href="{{ url('/admin/dashboard') }}">Home</a>
-                             @endrole
-                            @else
-                                <a href="{{ url('/login') }}">Login</a> | 
-                                <a href="{{ url('/register') }}">Register</a>
-                            @endif
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-xs-12">
-                            <div class="searchbox">
-                                <input type="text" id="serch" class="search-box" placeholder="Search">
-                                <label for="serch" class="fa fa-search mainsearch"></label>
-                                <i class="fa fa-close close-button"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="header_bottom_area">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-sm-12">
-                            <div class="mainmenu">
-                                <nav>
-                                    <ul>
-                                        <li class="active"><a href="index.html">home</a></li>
-                                        <li><a href="about.html">about us</a></li>
-                                        <li><a href="service.html">our services</a></li>
-                                        <li><a href="contact.html">contact us</a></li>
-                                    </ul>
-                                </nav>
-                            </div>
-                        </div>
-                        <!-- column End -->
-                    </div>
-                    <!-- row End -->
-                </div>
-                <!-- container End -->
-            </div>
-            <!-- Header_Bottom End -->
-        </header>
-        <!-- Header End -->
-=======
 
     @include('frontend.header')
 
->>>>>>> 3ac3a968c9f808a4993ae25eca8397ffba7aa81d
 <!-- ==================================================
     PACKAGES
 =================================================== -->
