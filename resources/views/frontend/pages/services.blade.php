2af736c1c811952842ae98cddcea0073e6d93d06
Altrim <altr1m1@hotmail.com>
2017-03-29 22:23:34 +0200

    heade color
    heade color

--- resources/views/frontend/pages/services.blade.php
+++ resources/views/frontend/pages/services.blade.php
@@ -53,7 +53,6 @@
         <section class="romana_service_area">
             <div class="container">
                 <div class="row">
-                    <h2 style="color:#0072bb;font-weight:bold;text-align: center;border-bottom: #0072bb solid 2px;box-shadow: black 0 8px 21px -13px;">Basis</h2><br>
                     <div class="col-sm-4">
                         <div class="single_service text-center">
                             <img src="images/icon1.png" alt="">
@@ -121,7 +120,6 @@
                 </div>
 
                 <div class="row">
-                    <h2 style="color:#0072bb;font-weight:bold;text-align: center;border-bottom: #0072bb solid 2px;box-shadow: black 0 8px 21px -13px;">Ekstra</h2><br>
                     <div class="col-sm-4">
                         <div class="single_service text-center">
                             <img src="images/icon1.png" alt="">
@@ -202,7 +200,6 @@
                 </div>
                 
                 <div class="row">
-                    <h2 style="color:#0072bb;font-weight:bold;text-align: center;border-bottom: #0072bb solid 2px;box-shadow: black 0 8px 21px -13px;">Eksklusiv</h2><br>
                     <div class="col-sm-4">
                         <div class="single_service text-center">
                             <img src="images/icon1.png" alt="">
@@ -327,7 +324,6 @@
                     </div>
                 </div>
                 <div class="row">
-                    <h2 style="color:#0072bb;font-weight:bold;text-align: center;border-bottom: #0072bb solid 2px;box-shadow: black 0 8px 21px -13px;">Eksklusiv +</h2><br>
                     <div class="col-sm-4">
                         <div class="single_service text-center">
                             <img src="images/icon1.png" alt="">
