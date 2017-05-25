50e4b7e44e3498df88b5e7a33b1b2824a926103e
riatabduramani <riat.abduramani@gmail.com>
2017-05-25 15:06:13 +0200

    Redirect to panel after registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/panel';
+    protected $redirectTo = '/'.\App::getLocale().'/panel';
 
     /**
      * Create a new controller instance.
