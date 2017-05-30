834df4cfec7f99033e45b3689f605bc836dc01f5
riatabduramani <riat.abduramani@gmail.com>
2017-05-30 09:24:09 +0200

    login & register language added

--- resources/views/frontend/header.blade.php
+++ resources/views/frontend/header.blade.php
@@ -13,20 +13,6 @@
                                 <ul>
                                     <li><span class="fa fa-phone"></span>{{ $settings->phone }}</li>
                                     <li><span class="fa fa-envelope"></span>{{ $settings->email }}</li>
-                                      
-                                    <!--@foreach (Config::get('app.languages') as $lang => $name)
-                                        @if ($lang == App::getLocale())
-                                            <li class="active"><span lang="{{ $lang }}" style="color: #888">{{ $name }}</span></li>
-                                        @else
-                                            <?php
-                                                $url = URL::to($lang);
-                                                $url = str_replace(App::getLocale(), $lang, env('APP_URL').'/'.$lang.'/'.Request::segment(2).'/'.Request::segment(3).'/'.Request::segment(4));
-                                            ?>
-                                            <li class="inactive"><a lang="{{ $lang }}" href="{{ $url }}">{{ $name }}</a></li>
-                                        @endif
-                                    @endforeach-->
-            
-                                    <!--<li><span class="fa fa-map-marker"></span></li>-->
                                 </ul>
                             </div>
                         </div>
@@ -41,22 +27,13 @@
                                 <a href="/{{ App::getLocale() }}/panel"><i class="fa fa-tachometer"></i> @lang('front.clientdashboard')</a>
                                 @endrole
                             @else
-            <a href="{{ url('/login') }}"><i class="fa fa-user"></i>&nbsp @lang('front.login')</a> | 
+            <a href="/{{ App::getLocale() }}/login"><i class="fa fa-user"></i>&nbsp @lang('front.login')</a> | 
             <!--<a href=""><i class="fa fa-user"></i>&nbsp Login</a> | -->
-            <a href="{{ url('/register') }}">@lang('front.register')</a>
+            <a href="/{{ App::getLocale() }}/register">@lang('front.register')</a>
                             @endif
                             </div>
                         </div>
                     </div>
-                    <!--<div class="row">
-                        <div class="col-xs-12">
-                            <div class="searchbox">
-                                <input type="text" id="serch" class="search-box" placeholder="Search">
-                                <label for="serch" class="fa fa-search mainsearch"></label>
-                                <i class="fa fa-close close-button"></i>
-                            </div>
-                        </div>
-                    </div>-->
                 </div>
             </div>
             <div class="header_bottom_area">
