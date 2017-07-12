2ba6f9f84e3b255bcb076dfa09b1c142280ab92e
riatabduramani <riat.abduramani@gmail.com>
2017-07-12 23:14:58 +0200

    Changing kernel task scheduling

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,6 +27,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
         $schedule->command('task:repeat')->dailyAt('2:00');
+        
     }
 
     /**
