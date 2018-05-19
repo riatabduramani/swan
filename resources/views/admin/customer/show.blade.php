332c12d46da3e4e5492a274e9b615f3335bd913f
Altrim <altr1m1@hotmail.com>
2018-05-19 20:06:56 +0200

    input file from mobile

--- resources/views/admin/customer/show.blade.php
+++ resources/views/admin/customer/show.blade.php
@@ -92,7 +92,7 @@
 <?php   if(stristr(strtolower($_SERVER['HTTP_USER_AGENT']),'android') || strstr(strtolower($_SERVER['HTTP_USER_AGENT']),'android')) { ?>
 <!--<button onclick="importLibrary(event);">Import from Library</button> -->
 <!--<input type="file" id="attach" onclick="importLibrary(event);" accept="image/*" capture="filesystem" rel="external" placeholder="Attach" name="attach" class="" required/>-->
-<a href="" onclick="importLibrary('http://swan.mk/admin/customer/{{Auth::user()->id}}' )">Add Photo</a>
+<a href="" onclick="importLibrary('http://swan.mk/admin/customer/{{$customer->id}}' )">Add Photo</a>
 <?php } else { ?>
  <input type="file" id="attach" accept="image/*" capture="filesystem" rel="external" placeholder="Attach" name="attach" class="" required/>
  <?php  } ?>   
